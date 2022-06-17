4148320d3d1dfb5a2ee363df778aa701103a0240
Waddiii <106839391+Waddiii@users.noreply.github.com>
2022-06-17 15:12:49 +0300

    Add files via upload

---
+++ dist/precache-manifest.8996c2aef3f3ee6958b84836de84869d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1a3308c6be9b70c473362548311b08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c004f0f2bd43887e6a6c",
+    "url": "/js/app.b64885dd.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
