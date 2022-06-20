b1f4afca88a33f9fca0d4e72a225f1ddc71a1778
Waddiii <106839391+Waddiii@users.noreply.github.com>
2022-06-20 17:28:42 +0300

    Add files via upload

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+   <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand">Marvel</a>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Поиск..." aria-label="Поиск" v-model="search"
+          @input="changeSearch(search)"
+          >
+          <button class="btn btn-outline-danger"  type="submit" @click="() => {search = ''; changeSearch(search)}">Все</button>
+        </form>
+      </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        props: ["changeSearch"],
+        data() {
+            return {
+              search: '',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
